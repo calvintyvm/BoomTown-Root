513fdcb15cfba9880d0c26943043e70f65d13b99
Calvin Ting <calvinting@protonmail.com>
2018-05-03 19:46:29 -0700

    added material ui for sharing form, check for items not yet implemented

--- client/src/containers/Share/Share.js
+++ client/src/containers/Share/Share.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react';
 import { Form, Field } from 'react-final-form';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
+import {
+    Step,
+    Stepper,
+    StepLabel,
+    StepContent,
+  } from 'material-ui/Stepper';
+  import RaisedButton from 'material-ui/RaisedButton';
+  import FlatButton from 'material-ui/FlatButton';
+  import TextField from 'material-ui/TextField';
+  import './styles.css';
+
+  const styles = {
+
+    underlineStyle: {
+      borderColor: 'black'
+    },
+    floatingLabelStyle: {
+        color: 'white'
+    }
+  };
+
 class Share extends Component {
+    state = {
+        finished: false,
+        stepIndex: 0,
+      };
+
     onSubmit(formValues) {
         console.log('Form was submitted:', formValues);
       }
@@ -12,60 +37,136 @@ class Share extends Component {
         console.log('Validating:', formValues);
       }
 
-      render() {
+      handleNext = () => {
+        const { stepIndex } = this.state;
+        this.setState({
+          stepIndex: stepIndex + 1,
+          finished: stepIndex >= 2,
+        });
+      };
+
+      handlePrev = () => {
+        const { stepIndex } = this.state;
+        if (stepIndex > 0) {
+          this.setState({ stepIndex: stepIndex - 1 });
+        }
+      };
+
+      renderStepActions(step) {
+        const { stepIndex } = this.state;
+
         return (
-            <div className="container col-md-4">
-                <Form
-                    onSubmit={values => this.onSubmit(values)}
-                    validate={this.validate.bind(this)}
-                    render={({ handleSubmit, values }) => (
-                        <form
-                            onSubmit={e => {
-                      e.preventDefault();
-                      handleSubmit(values);
-                    }}
-                        >
-                            <div>
-                                <label> Add an Image </label>
-                                <p>We live in a visual culture. Upload an image of the item you're sharing.</p>
-                                <Field name="image">
-                                    {({ input, meta }) => <input type="file" {...input} />}
-                                </Field>
-                            </div>
-                            <div>
-                                <label>Add a Title & Description</label>
-                                <p>Title</p>
-                                <Field name="title">
-                                    {({ input, meta }) => <input type="text" {...input} />}
-                                </Field>
-                                <p>Description</p>
-                                <Field name="description">
-                                    {({ input, meta }) => <input type="text" {...input} />}
-                                </Field>
-                            </div>
-                            <div>
-                                <label>Categorize Your Item</label>
-                                <p />
-                                <Field name="tags">
-                                    {({ input, meta }) => (<SelectField floatingLabelText="Select Category Tags">
-                                        <MenuItem value={1} checked primaryText="Electronics" />
-                                        <MenuItem value={2} checked primaryText="Household Items" />
-                                        <MenuItem value={3} checked primaryText="Musical Instruments" />
-                                        <MenuItem value={4} checked primaryText="Physical Media" />
-                                        <MenuItem value={5} checked primaryText="Recreational Equipment" />
-                                        <MenuItem value={6} checked primaryText="Sporting Goods" />
-                                        <MenuItem value={7} checked primaryText="Tools" />
-                                    </SelectField>)}
-                                </Field>
-                            </div>
-                        </form>
-                )}
+            <div style={{ margin: '12px 0' }}>
+                <RaisedButton
+                    label={'Next'}
+                    disableTouchRipple
+                    disableFocusRipple
+                    onClick={this.handleNext}
+                    style={{ marginRight: 12 }}
                 />
-
-
+                {step > 0 && (
+                <RaisedButton
+                    label="Back"
+                    disabled={stepIndex === 0}
+                    disableTouchRipple
+                    disableFocusRipple
+                    secondary
+                    onClick={this.handlePrev}
+                />
+            )}
             </div>
         );
       }
+
+
+      render() {
+        const { finished, stepIndex } = this.state;
+        return (
+
+            <Form
+                onSubmit={values => this.onSubmit(values)}
+                validate={this.validate.bind(this)}
+                render={({ handleSubmit, values }) => (
+                    <form
+                        onSubmit={e => {
+                      e.preventDefault();
+                      handleSubmit(values);
+                    }}
+                    >
+                    <div>
+                        <Stepper activeStep={stepIndex} orientation="vertical">
+                            <Step>
+                                <StepLabel> Add an Image </StepLabel>
+                                <StepContent>
+                                    <p>We live in a visual culture. Upload an image of the item you're sharing.</p>
+                                    <Field name="image">
+                                        {({ input, meta }) => <input type="file" {...input} />}
+                                    </Field>
+                                    {this.renderStepActions(0)}
+                                </StepContent>
+                            </Step>
+                            <Step>
+                                <StepLabel>Add a Title & Description</StepLabel>
+                                <StepContent>
+                                <p>Folks need to know what you're sharing. Give them a clue by adding a title & description.</p>
+                                    <Field name="title">
+                                        {({ input, meta }) => (<TextField
+                                            hintText="Title"
+                                            floatingLabelText="Title"
+                                            floatingLabelFixed
+                                            underlineFocusStyle={styles.underlineStyle}
+                                            floatingLabelStyle={styles.floatingLabelStyle}
+                                            {...input}
+                                        />)}
+                                    </Field>
+                                    <p />
+                                    <Field name="description">
+                                        {({ input, meta }) => (<TextField
+                                            hintText="Description"
+                                            floatingLabelText="Description"
+                                            multiLine={true}
+                                            underlineFocusStyle={styles.underlineStyle}
+                                            floatingLabelStyle={styles.floatingLabelStyle}
+                                            rows={3}
+                                            {...input}
+                                        />)}
+                                    </Field>
+                                    {this.renderStepActions(2)}
+                                </StepContent>
+                            </Step>
+                            <Step>
+                                <StepLabel>Categorize Your Item</StepLabel>
+                                <StepContent>
+                                    <p />
+                                    <Field name="tags">
+                                        {({ input, meta }) => (<SelectField floatingLabelText="Select Category Tags">
+                                            <MenuItem value={1} checked primaryText="Electronics" />
+                                            <MenuItem value={2} checked primaryText="Household Items" />
+                                            <MenuItem value={3} checked primaryText="Musical Instruments" />
+                                            <MenuItem value={4} checked primaryText="Physical Media" />
+                                            <MenuItem value={5} checked primaryText="Recreational Equipment" />
+                                            <MenuItem value={6} checked primaryText="Sporting Goods" />
+                                            <MenuItem value={7} checked primaryText="Tools" />
+                                        </SelectField>)}
+                                    </Field>
+                                    {this.renderStepActions(2)}
+                                </StepContent>
+                            </Step>
+                            <Step>
+                                <StepLabel>Confirm Things!</StepLabel>
+                                <StepContent>
+                                    <p>Great! If you're happy with everything, tap the button.</p>
+                                    <RaisedButton type="submit" label="Confirm" />            
+                                </StepContent>
+                            </Step>
+                        </Stepper>
+                        </div>
+                    </form>
+
+                )}
+            />
+        );
+ }
 }
 
 
